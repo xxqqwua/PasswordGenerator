a88b4e9fcf3ec21736d5402804a8388d93549fce
xxqqwua <xxqqwua@gmail.com>
2024-10-17 23:12:47 +0300

    added the function mixed_register_characters

--- passwordprofiles.py
+++ passwordprofiles.py
@@ -1,19 +1,29 @@
 from passwordoptions import *
+import random
+import pyperclip
+
+
+def mixed_register_characters(length):
+    lower = upper_n_lower_register_characters(length/2, "lowercase")
+    upper = upper_n_lower_register_characters(length/2, "uppercase")
+    lower.extend(upper)
+    random.shuffle(lower)
+    return lower
+
 
 # Avoid numbers and special characters
-def easy_to_say(length):
-    uppercase()
-    lowercase()
+def easy_to_remember(length):
+    password = ""
 
+    for el in mixed_register_characters(length):
+        password += el
 
-# Avoid ambiguous characters like l, 0, 1, etc
-def easy_to_read(length):
-    pass
+    pyperclip.copy(password)
+    print(password)
 
 
 # Any character combination like !, @, #, etc
 def all_characters(length):
-    uppercase()
-    lowercase()
+    mixed_register_characters(length)
     numbers()
     symbols()

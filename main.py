a3705db360564debf773afd58c2827d34dd42727
xxqqwua <xxqqwua@gmail.com>
2024-10-17 23:31:11 +0300

    refactored text in user choose and add custom to elif

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ def main():
             print('Invalid input! Please enter a number.')
             continue
 
-        user_choose = input("Choose a profile (1. Easy to remember 2. Strong (0 - Exit)): ")
+        user_choose = input("Choose a profile (1. Easy to remember 2. Strong 3. Custom (0 - Exit)): ")
 
         if 'easy' in user_choose or '1' in user_choose:
             easy_to_remember(length)
             password = True
         elif 'strong' in user_choose or '2' in user_choose:
             all_characters(length)
+            password = True
+        elif 'custom' in user_choose or '3' in user_choose:
+            custom(length)
             password = True
         elif user_choose == '0':
             break

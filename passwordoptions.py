2fbe0d9dd73a9c0901f8c52df800c9dde73675bd
xxqqwua <xxqqwua@gmail.com>
2024-10-17 19:41:06 +0300

    Code Optimisation

--- passwordoptions.py
+++ passwordoptions.py
@@ -1,22 +1,30 @@
 import random
 
-# Uppercase characters in password
-def uppercase():
-    letter = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    return letter
 
+# Uppercase 'n' lowercase characters in password
+def upper_n_lower_register_characters(length, register):
+    def create_characters(length, roster, characters):
+        while len(characters) < length:
+            letter = random.choice(roster)
+            characters.append(letter)
+        return characters
 
-# Lowercase characters in password
-def lowercase():
-    letter = random.choice("abcdefghijklmnopqrstuvwxyz")
-    return letter
+    if register == "uppercase":
+        uppercases_characters = []
+        create_characters(length, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", uppercases_characters)
+        return uppercases_characters
+
+    elif register == "lowercase":
+        lower_characters = []
+        create_characters(length, "abcdefghijklmnopqrstuvwxyz", lower_characters)
+        return lower_characters
 
 
 # Numbers in password
-def numbers():
+def numbers(length):
     pass
 
 
 # Symbols in password
-def symbols():
+def symbols(length):
     pass

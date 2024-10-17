a2877ff8deca17f62b0489d2b0833e8780b51c68
xxqqwua <xxqqwua@gmail.com>
2024-10-17 23:11:31 +0300

    added the main function

---
+++ main.py
@@ -0,0 +1,28 @@
+from passwordprofiles import *
+
+def main():
+    password = False
+    while not password:
+        try:
+            length = int(input("Enter the length of the password: "))
+        except ValueError:
+            print('Invalid input! Please enter a number.')
+            continue
+
+        user_choose = input("Choose a profile (1. Easy to remember 2. Strong 4. Exit): ")
+
+        if 'easy' in user_choose or '1' in user_choose:
+            easy_to_remember(length)
+            password = True
+        elif 'strong' in user_choose or '2' in user_choose:
+            all_characters(length)
+            password = True
+        elif user_choose == '4':
+            break
+        else:
+            print('Invalid input')
+            continue
+
+
+if __name__ == '__main__':
+    main()
